0dadf39745790c29c8b6bbdaa01e6d66777e8404
Oliver Keyes <ironholds@gmail.com>
2014-01-30 09:24:39 -0800

    Alter hive query to select for Special:BannerRandom

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 MobileSessions <- function(){
   
   #Grab the data from hive
-  query_log <- system(paste("hive --auxpath /usr/lib/hcatalog/share/hcatalog/hcatalog-core-0.5.0-cdh4.3.1.jar --database wmf -e '
+  query_log <- system(paste('hive --auxpath /usr/lib/hcatalog/share/hcatalog/hcatalog-core-0.5.0-cdh4.3.1.jar --database wmf -e "
                 SELECT dt,
                   ip,
                   uri_host,
@@ -11,8 +11,9 @@ MobileSessions <- function(){
                   referer,
                   user_agent
                 FROM webrequest_mobile 
-                WHERE year >= ",start_year,"
-                  AND month >= ",start_month,"' > ",mobile_file, sep = ""))
+                WHERE year >= ',start_year,'
+                  AND month >= ',start_month,'"
+                  AND uri_path LIKE \"%Special:BannerRandom%\";"> ',mobile_file, sep = ""))
   
   #After the data _has_ been grabbed, read it in
   data.df <- read_data(mobile_file)
