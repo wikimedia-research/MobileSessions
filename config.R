756f50b96dd0cc96341a4b719fe1191f9c494e96
Oliver Keyes <ironholds@gmail.com>
2014-02-02 21:15:07 -0800

    modify file locations

--- config.R
+++ config.R
@@ -10,6 +10,6 @@ start_year <- 2014
 start_month <- 1
 
 #Files
-mobile_file <- file.path(getwd(),"mobile_data.tsv") #Address of the hive output file.
-resulting_file <- file.path(getwd(),"resulting_data.tsv") #Address of the save file.
-metadata_file <- file.path(getwd(),"metadata.tsv") #Metadata about the run
+mobile_file <- file.path(getwd(),"Data","mobile_data.tsv") #Address of the hive output file.
+resulting_file <- file.path(getwd(),"Data","resulting_data.tsv") #Address of the save file.
+metadata_file <- file.path(getwd(),"Data","metadata.tsv") #Metadata about the run
