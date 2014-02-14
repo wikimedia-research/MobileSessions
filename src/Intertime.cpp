5ddac42bf40560d5d0eaefded5287ccb7c494d63
Oliver Keyes <ironholds@gmail.com>
2014-02-13 16:24:26 -0800

    first piece of productionised C++. Woo!

---
+++ src/Intertime.cpp
@@ -0,0 +1,23 @@
+#include <Rcpp.h>
+#include <vector>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector intertime(NumericVector x) {
+  
+  //Identify size of input object
+  int input_size = x.size();
+  
+  //Instantiate resulting object
+  NumericVector output_vector(input_size-1);
+
+  //Loop over the data
+  for(int i = 1; i < input_size;++i){
+    
+    //For each entry, the output value is [entry] minus [previous entry]
+    output_vector[i-1] = (x[i] - x[i-1]);
+  }
+  
+  //Return
+  return output_vector;
+}
